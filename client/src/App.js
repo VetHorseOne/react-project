663250ecb3c59c6886a775fb352bb5066a23a0b9
Ygor Cortês <129232028+YgorNC@users.noreply.github.com>
2024-11-22 16:46:13 -0300

    react-project

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../components/Home";
+import Cadastro from "..components/Cadastro";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Cadastro />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
